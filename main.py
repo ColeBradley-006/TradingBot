a031c400304dc79df38480b0b1ce9254ccaaeacd
ColeBradley-006 <85534478+ColeBradley-006@users.noreply.github.com>
2022-01-15 18:13:24 -0500

    "Added Stock List"

--- main.py
+++ main.py
@@ -1,19 +1,13 @@
-# This is a sample Python script.
-import numpy
-import pandas
-import matplotlib
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import numpy as np
+import pandas as pd
+import matplotlib as mp
+import yfinance as yf
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+# This makes sure the program doesn't crash by attempting to divide by 0
+def zero_division(a, b):
+    return a / b if b != 0 else 0
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    print("Cool")
